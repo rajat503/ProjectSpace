e8ffce21244c993f557865b43aab84f3e687cf09
Rajat Agarwal <rajat503@gmail.com>
2014-11-09 14:52:39 +0530

    View all projects

---
+++ viewall.php
@@ -0,0 +1,28 @@
+<?php
+require 'connect.php';
+//include 'options.php';
+$query="SELECT * FROM `projects`";
+if($query_run=mysql_query($query))
+{
+	$query_num_rows=mysql_num_rows($query_run);
+	if($query_num_rows==0)
+	{
+		echo "No projects listed.";
+	}
+	for($a=0;$a<$query_num_rows;$a++)
+	{
+				
+		$serial=mysql_result($query_run,$a,'serial');
+		echo mysql_result($query_run,$a,'title').'<br/>';
+		echo mysql_result($query_run,$a,'details').'<br/>';
+		echo mysql_result($query_run,$a,'prereq').'<br/>';
+		echo mysql_result($query_run,$a,'number').'<br/>';
+		echo mysql_result($query_run,$a,'name').'<br/>';
+		echo mysql_result($query_run,$a,'contact').'<br/>';
+	}
+}
+else
+{
+	echo "Query Failed.";
+}				
+?>
